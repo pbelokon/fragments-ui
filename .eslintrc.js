e9057a6173d437fe6945e77d6f13d91531d6d813
Pavel Belokon <pbelokon@myseneca.ca>
2023-07-14 14:02:59 -0400

    added eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// .eslintrc.js
+
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {},
+};
